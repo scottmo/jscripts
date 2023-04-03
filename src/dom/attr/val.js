31d0bca3445862fcf20c79006cc1c0a4c4970096
scottmo <scottscmo@gmail.com>
2023-04-03 13:07:31 -0500

    fix: input value setter

--- src/dom/attr/val.js
+++ src/dom/attr/val.js
@@ -1,10 +1,16 @@
 $.val = function(node, value) {
+    function setInputProp(input, prop, value) {
+        // to avoid triggering custom logic
+        Object.getOwnPropertyDescriptor(HTMLInputElement.prototype, prop).set.call(input, value);
+    }
     node = $(node);
     if (node.nodeName === "INPUT") {
         if (node.getAttribute("type") === "checkbox") {
-            node.checked = value;
-            return;
+            setInputProp(node, "checked", value);
+        } else {
+            setInputProp(node, "value", value);
         }
+    } else {
+        node.value = value;
     }
-    node.value = value;
 }
